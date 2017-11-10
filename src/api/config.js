7c38c8cbdd6bf694c4f0a73e91eccdfbabac125f
System Administrator <root@MacBook-Pro-de-Carlos-Leon.local>
2017-11-10 11:48:28 -0600

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+export default {
+	appname: 'platzimusicleon',
+	apiKey: '0e31d1d3c0b8ee8a100c41298ebec05e',
+	secret: '62718fb5158e4117bbc73737faf9ceb8',
+	registeredTo: 'Carleon4568'
+}
+
+
+
+//Application name	platzimusicleon
+//API key	0e31d1d3c0b8ee8a100c41298ebec05e
+//Shared secret	62718fb5158e4117bbc73737faf9ceb8
+//Registered to	Carleon4568
